e24e4e002d2559b46f1a5f3fe2b1ca6a63b28b45
Azure pipeline <you@example.com>
2020-07-13 13:20:24 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Prevalence.js
+++ alspac/mesh/Prevalence.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2001',1],]);var citations =([['Year', 'Number of Citations'],['2001',0],]);
+var papers =([['Year', 'Number of papers'],['2001',1],['2002',0],['2003',0],['2004',0],['2005',1],]);var citations =([['Year', 'Number of Citations'],['2001',0],['2002',0],['2003',0],['2004',0],['2005',99],]);
